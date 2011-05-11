cfcde5b4bd4a313c7944af850c4fcd4713a2241e
Markus Törnqvist <mjt+illyria@skyhood.com>
2011-05-11 19:43:43 +0300

    Translation

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -10,7 +12,7 @@ def leave_feedback(request, template_name='feedback/feedback_form.html'):
         feedback = form.save(commit=False)
         feedback.user = request.user
         feedback.save()
-        request.user.message_set.create(message="Your feedback has been saved successfully.")
+        request.user.message_set.create(message=_("Your feedback has been saved successfully."))
         return HttpResponseRedirect(request.POST.get('next', request.META.get('HTTP_REFERER', '/')))
     return render_to_response(template_name, {'form': form}, context_instance=RequestContext(request))
 

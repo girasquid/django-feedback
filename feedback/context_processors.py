92fb1ec252ffe7a36695e701bdd8c68df71546da
Markus Törnqvist <mjt+illyria@skyhood.com>
2011-09-05 14:43:41 +0300

    Context processor

--- feedback/context_processors.py
+++ feedback/context_processors.py
@@ -1,7 +1,11 @@
-from feedback.forms import FeedbackForm
+from feedback.forms import FeedbackForm, AnonymousFeedbackForm
 
 def feedback_form(request):
     feedback_form = None
     if request.user.is_authenticated():
         feedback_form = FeedbackForm()
+    else:
+        feedback_form = AnonymousFeedbackForm()
+
     return {'feedback_form': feedback_form}
+

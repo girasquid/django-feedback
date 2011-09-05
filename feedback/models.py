770d90aba014e1ab16622efd1dc0d76a5a52e161
Markus Törnqvist <mjt+illyria@skyhood.com>
2011-09-05 14:42:44 +0300

    Separate class for allowing anonymous feedback

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,5 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
 from django.utils.translation import ugettext_lazy as _
 
 from django.db import models
@@ -5,13 +7,11 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
-class Feedback(models.Model):
-    
+class BaseFeedback(models.Model):
     class Meta:
-        
+        abstract = True
         ordering = ['-time']
-    
-    user    = models.ForeignKey(User, verbose_name=_('User'))
+
     type    = models.CharField(choices=settings.FEEDBACK_CHOICES, max_length=100, verbose_name=_('Type'))
     message = models.TextField(verbose_name=_('Message'))
     time    = models.DateTimeField(auto_now_add=True, verbose_name=_('Time'))
@@ -21,3 +21,14 @@ class Feedback(models.Model):
 
     def get_absolute_url(self):
         return reverse('admin:view-feedback', args=[self.id])
+
+
+class Feedback(BaseFeedback):
+    user = models.ForeignKey(User, verbose_name=_('User'))
+
+
+class AnonymousFeedback(BaseFeedback):
+    user = models.ForeignKey(User, verbose_name=_('User'), null=True, blank=True, default=None)
+
+# EOF
+
